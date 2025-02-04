167d6333a62ea72ade42a478f41ff497a76eb64b
Andres <andres.amc2@gmail.com>
2025-02-03 22:51:20 -0600

    Agregando PWA y Service Worker

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "iglesia-cache-v1";
+const urlsToCache = [
+    "index.html",
+    "estadisticas_culto.html",
+    "conteo_evangelismo.html",
+    "ventas_jovenes.html",
+    "css/styles.css",
+    "js/conteo_evangelismo.js",
+    "js/ventas_jovenes.js",
+    "img/logo.png",
+    "img/iglesia.jpg"
+];
+
+// Instalar Service Worker y cachear archivos
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Interceptar las solicitudes y responder desde la caché si es posible
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
